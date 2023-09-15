f06897ec91acd802f21ea86e4e85542ffbb9dfa9
ali.ejaz@xloopdigital.com <ali.ejaz@xloopdigital.com>
2023-09-15 18:37:15 +0500

    helloworld

--- src/main/java/ali/mine/hellowworld/HelloController.java
+++ src/main/java/ali/mine/hellowworld/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.service.annotation.GetExchange;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "Hi this is ali from BALF";
+        return "Hi this is ali";
     }
 }
